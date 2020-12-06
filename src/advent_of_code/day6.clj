62585a8cec943e425209515d795dd981e8f8850e
Hilton Pintor <hiltonpintor@gmail.com>
2020-12-06 13:21:37 -0300

    Day 6 pt 2

--- src/advent_of_code/day6.clj
+++ src/advent_of_code/day6.clj
@@ -1,7 +1,7 @@
 (ns advent-of-code.day6
   (:require [advent-of-code.inputs.input-reader :as ir]))
 
-(defn sum-yes-questions [lines]
+(defn sum-anyone-yes-questions [lines]
   (loop [index 0
          current-group-answer ""
          current-sum 0]
@@ -19,6 +19,41 @@
 (defn part-1 []
   (ir/with-lines "src/advent_of_code/inputs/day6.txt"
                  (->> lines
-                      sum-yes-questions)))
+                      sum-anyone-yes-questions)))
 
 (part-1)
+
+(defn count-occurrences [char string]
+  (reduce #(if (= char %2) (inc %1) %1) 0 string))
+
+(defn count-questions-everyone-answered-yes [group-answer num-people]
+  (let [questions-answered (distinct group-answer)]
+    (reduce
+      #(if (= (count-occurrences %2 group-answer) num-people)
+         (inc %1)
+         %1)
+      0
+      questions-answered)))
+
+(defn sum-everyone-yes-questions [lines]
+  (loop [index 0
+         current-group-answer ""
+         num-people 0
+         current-sum 0]
+
+    (cond
+      (= index (count lines))
+      (+ current-sum (count-questions-everyone-answered-yes current-group-answer num-people))
+
+      :else
+      (let [current-line (nth lines index)]
+        (if (empty? current-line)
+          (recur (inc index) "" 0 (+ current-sum (count-questions-everyone-answered-yes current-group-answer num-people)))
+          (recur (inc index) (str current-group-answer current-line) (inc num-people) current-sum))))))
+
+(defn part-2 []
+  (ir/with-lines "src/advent_of_code/inputs/day6.txt"
+                 (->> lines
+                      sum-everyone-yes-questions)))
+
+(part-2)
