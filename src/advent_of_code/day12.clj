c6e8277cf480144968a58d91ee56de3d69093de2
Hilton Pintor <hiltonpintor@gmail.com>
2020-12-20 13:55:58 -0300

    day 12 pt 1

---
+++ src/advent_of_code/day12.clj
@@ -0,0 +1,58 @@
+(ns advent-of-code.day12
+  (:require [advent-of-code.inputs.input-reader :as ir]))
+
+; PART 1
+; What is the Manhattan distance between that location and the ship's starting position?
+(defn parse-line [line]
+  {:action (first line) :amount (Integer/parseInt (apply str (rest line)))})
+
+(defn parse-input [input]
+  (map parse-line input))
+
+(def directions [\E \S \W \N])
+
+(defn direction? [action]
+  (contains? (set directions) action))
+
+(defn new-direction-index [current-direction action amount]
+  (let [current-direction-index (.indexOf directions current-direction)]
+    (if (= action \L)
+      (mod (- current-direction-index (/ amount 90)) (count directions))
+      (mod (+ current-direction-index (/ amount 90)) (count directions)))))
+
+
+(defn change-direction [movements action amount]
+  (let [current-direction (:current-direction movements)
+        new-direction-index (new-direction-index current-direction action amount)
+        new-direction (nth directions new-direction-index)
+        movements-with-new-direction (assoc movements :current-direction new-direction)]
+    movements-with-new-direction))
+
+(defn move [movements {:keys [action amount]}]
+  (cond
+    (direction? action)
+    (update movements action #(+ % amount))
+
+    (= action \F)
+    (update movements (:current-direction movements) #(+ % amount))
+
+    :else
+    (change-direction movements action amount)))
+
+(defn move-ship [parsed-input]
+  (let [initial-state {\N 0 \W 0 \E 0 \S 0 :current-direction \E}]
+    (reduce move initial-state parsed-input)))
+
+(defn manhattan-distance [movements]
+  (+
+    (Math/abs (- (get movements \N) (get movements \S)))
+    (Math/abs (- (get movements \E) (get movements \W)))))
+
+(defn part-1 []
+  (ir/with-lines "src/advent_of_code/inputs/day12.txt"
+                 (->> lines
+                      parse-input
+                      move-ship
+                      manhattan-distance)))
+
+(part-1)
