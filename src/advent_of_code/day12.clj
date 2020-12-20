21994b4274cdf7fdf7b61d8f5a431bc3e154bb33
Hilton Pintor <hiltonpintor@gmail.com>
2020-12-20 15:22:28 -0300

    day 12 pt 2

--- src/advent_of_code/day12.clj
+++ src/advent_of_code/day12.clj
@@ -20,11 +20,15 @@
       (mod (- current-direction-index (/ amount 90)) (count directions))
       (mod (+ current-direction-index (/ amount 90)) (count directions)))))
 
+(defn new-direction [action amount current-direction]
+  (->> current-direction
+       (new-direction-index action amount)
+       (nth directions)))
+
 (defn change-direction [movements action amount]
   (->> movements
        :current-direction
-       (new-direction-index action amount)
-       (nth directions)
+       (new-direction action amount)
        (assoc movements :current-direction)))
 
 (defn move [movements {:keys [action amount]}]
@@ -55,3 +59,43 @@
                       manhattan-distance)))
 
 (part-1)
+
+
+; PT 2
+(defn move-forward-correctly [movements amount]
+  (let [waypoint (seq (:waypoint movements))
+        amounts-to-move (map #(assoc % 1 (* amount (last %))) waypoint)]
+    (reduce #(update %1 (first %2) + (last %2)) movements amounts-to-move)))
+
+(defn change-waypoint-direction [movements action amount]
+  (let [waypoint-seq (seq (:waypoint movements))
+        new-wp-seq (map (fn [curr-wp]
+                          [(new-direction action amount (first curr-wp))
+                           (last curr-wp)]) waypoint-seq)
+        new-wp (into (sorted-map) new-wp-seq)]
+    (assoc movements :waypoint new-wp)))
+
+(defn move-correctly [movements {:keys [action amount]}]
+  (cond
+    (direction? action)
+    (update-in movements [:waypoint action] #(+ % amount))
+
+    (= action \F)
+    (move-forward-correctly movements amount)
+
+    :else
+    (change-waypoint-direction movements action amount)))
+
+
+(defn move-ship-correctly [parsed-input]
+    (let [initial-state {\N 0 \W 0 \E 0 \S 0 :waypoint {\E 10 \N 1 \S 0 \W 0}}]
+      (reduce move-correctly initial-state parsed-input)))
+
+(defn part-2 []
+  (ir/with-lines "src/advent_of_code/inputs/day12.txt"
+                 (->> lines
+                      parse-input
+                      move-ship-correctly
+                      manhattan-distance)))
+
+(part-2)
