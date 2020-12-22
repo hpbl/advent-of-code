61cc47b90fe5486e113c16650c5ded78d667e697
Hilton Pintor <hiltonpintor@gmail.com>
2020-12-22 10:51:10 -0300

    day 13 pt 2

--- src/advent_of_code/day13.clj
+++ src/advent_of_code/day13.clj
@@ -2,10 +2,6 @@
   (:require [clojure.string :as str]
             [advent-of-code.inputs.input-reader :as ir]))
 
-(def input [
-            "939"
-            "7,13,x,x,59,x,31,19"])
-
 (defn parse-input [input]
   {:my-arrival (Integer/parseInt (first input))
    :bus-ids (map #(Integer/parseInt %) (filter #(not= "x" %) (str/split (last input) #",")))})
@@ -16,14 +12,44 @@
                       :close-arrival (int (* (Math/ceil (/ my-arrival id)) id))}))
        (sort-by :close-arrival)))
 
-(map #('(% %)) [1 2 3])
-
-(defn part-2 []
+(defn part-1 []
   (ir/with-lines "src/advent_of_code/inputs/day13.txt"
                  (let [parsed-input (parse-input lines)
                        my-arrival (:my-arrival parsed-input)
                        close-arrivals (close-arrivals parsed-input)
                        {:keys [id close-arrival]} (first close-arrivals)]
                    (* id (- close-arrival my-arrival)))))
+
+(part-1)
+
+; PART 2:
+; What is the earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the list?
+(defn parse-input-2 [input]
+  (let [split-input (str/split (last input) #",")]
+    (remove nil? (map-indexed
+                   (fn [index bus] (cond (not= bus "x") {:bus (Integer/parseInt bus) :offset index}))
+                   split-input))))
+
+(defn find-time [parsed-input]
+  (loop [step (:bus (first parsed-input))
+         time 0
+         index 1]
+
+    (cond
+      (= index (count parsed-input))
+      time
+
+      :else
+      (let [{:keys [bus offset]} (nth parsed-input index)
+            bus-will-pass? (= (mod (+ time offset) bus) 0)]
+        (if bus-will-pass?
+          (recur (* step bus) time (inc index))
+          (recur step (+ time step) index))))))
+
+(defn part-2 []
+  (ir/with-lines "src/advent_of_code/inputs/day13.txt"
+                 (->> lines
+                      parse-input-2
+                      find-time)))
 
 (part-2)
